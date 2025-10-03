e276907646ad980a013c035aba33c2e7d63ee86f
GurruKey <SteamGurru@gmail.com>
2025-10-03 12:18:30 +0300

    Move transactions history block to transactions page

--- dodepus-casino/src/pages/Admin/role-edit/RoleEdit.jsx
+++ dodepus-casino/src/pages/Admin/role-edit/RoleEdit.jsx
@@ -1,6 +1,5 @@
-import { Col, Row, Stack } from 'react-bootstrap';
+import { Stack } from 'react-bootstrap';
 import EditRolePermissions from '../roles/blocks/EditRole/EditRolePermissions.jsx';
-import TransactionsHistory from '../roles/blocks/Transactions/TransactionsHistory.jsx';
 import VerificationQueue from '../roles/blocks/Verification/VerificationQueue.jsx';
 import ModerationChat from '../roles/blocks/Chat/ModerationChat.jsx';
 
@@ -8,16 +7,7 @@ export default function RoleEdit() {
   return (
     <Stack gap={3}>
       <EditRolePermissions />
-
-      <Row xs={1} xl={2} className="g-3">
-        <Col>
-          <TransactionsHistory />
-        </Col>
-        <Col>
-          <VerificationQueue />
-        </Col>
-      </Row>
-
+      <VerificationQueue />
       <ModerationChat />
     </Stack>
   );
