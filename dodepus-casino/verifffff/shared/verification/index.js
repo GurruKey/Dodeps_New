a855a4d9b9512fb7895ccf50f1a1589c13af2fa9
venssse <veo3demostar@gmail.com>
2025-10-08 07:29:43 +0300

    1

--- dodepus-casino/verifffff/shared/verification/index.js
+++
@@ -1,9 +0,0 @@
-export {
-  VERIFICATION_MODULES,
-  VERIFICATION_STATUS_LABELS,
-  deriveModuleStatesFromRequests,
-  summarizeModuleStates,
-  buildVerificationTimeline,
-  formatModuleList,
-} from './modules.js';
-export { useVerificationModules } from './useVerificationModules.js';
