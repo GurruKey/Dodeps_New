181f45faf438c1bbb97c1977d0d1555212415201
GurruKey <SteamGurru@gmail.com>
2025-10-12 19:21:31 +0300

    cleanup: purge fake local-sim data

--- dodepus-casino/local-sim/admin/features/promo/listLogs.js
+++ dodepus-casino/local-sim/admin/features/promo/listLogs.js
@@ -1,11 +1,1 @@
-export const PROMOCODES_LOGS = Object.freeze([
-  {
-    id: 'LOG-2024-0009',
-    adminId: 'ADM-003',
-    adminName: 'Мария Лебедева',
-    role: 'analyst',
-    section: 'promocodes',
-    action: 'Проанализировала эффективность промокода WELCOME-50',
-    createdAt: '2024-04-15T12:25:00.000Z',
-  },
-]);
+export const PROMOCODES_LOGS = Object.freeze([]);
