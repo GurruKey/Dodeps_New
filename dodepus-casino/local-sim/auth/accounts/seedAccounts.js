e66188705547ca5101db51ca4ff044ff3daeac82
GurruKey <SteamGurru@gmail.com>
2025-10-14 15:35:59 +0300

    fix: remove finances from owner preset

--- dodepus-casino/local-sim/auth/accounts/seedAccounts.js
+++ dodepus-casino/local-sim/auth/accounts/seedAccounts.js
@@ -1,5 +1,10 @@
 import { enrichAccountsWithTransactions } from './transactionsSeed';
+import { OWNER_PRESET_ACCOUNT } from './presets/ownerAccount';
 
 const BASE_ACCOUNTS = Object.freeze([]);
 
-export const PRESET_ACCOUNTS = Object.freeze(enrichAccountsWithTransactions([...BASE_ACCOUNTS]));
+const BUILTIN_ACCOUNTS = Object.freeze([OWNER_PRESET_ACCOUNT]);
+
+export const PRESET_ACCOUNTS = Object.freeze(
+  enrichAccountsWithTransactions([...BASE_ACCOUNTS, ...BUILTIN_ACCOUNTS]),
+);
